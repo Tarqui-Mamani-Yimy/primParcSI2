5ccb97f0d265d78ec4d5ca566fbd60c11075e2d3
Tarqui-Mamani-Yimy <yimyt771@gmail.com>
2026-09-05 10:50:02 -0400

    mas cambios

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,6 +10,7 @@ engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
+
 class Base(DeclarativeBase):
     pass
 
