0f98f849d02e4b72f57637fc241d52c5c4f48da9
Tarqui-Mamani-Yimy <yimyt771@gmail.com>
2026-09-06 11:44:32 -0400

    produccion

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: (import.meta as any).env?.VITE_API_URL || 'https://primparcsi2.onrender.com'
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000',
+  production: true,
+  apiUrl: 'http://localhost:8000' || 'https://primparcsi2.onrender.com',
 };
